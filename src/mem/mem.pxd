e1923641be41b1354e168b7a7d7ef0f38c35efee
Dennis H <dhilhorst2000@gmail.com>
2021-04-30 15:42:56 +0200

    add memory read/write basics

--- src/mem/mem.pxd
+++ src/mem/mem.pxd
@@ -1,27 +1,67 @@
-cimport cython 
+cimport cython
+from libcpp cimport bool
 
+cdef class MEM
+
+ctypedef unsigned char (*read_callback)(MEM mem)
+ctypedef void (*write_callback)(MEM mem, unsigned char value)
+
+
+cdef union read_ptr_t:
+    unsigned char* data
+    read_callback callback
+
+cdef union write_ptr_t:
+    unsigned char* data
+    write_callback callback
+
+cdef packed struct MemoryEntry:
+    read_ptr_t read_ptr
+    write_ptr_t write_ptr
+    
+    bool read
+    bool write
 
 cdef inline unsigned short read16(unsigned char* ptr):
     return (<unsigned short*>ptr)[0]
 
 @cython.final
 cdef class MEM:
-    cdef public:
-        unsigned char BOOT[0x100]
-        unsigned char ROM0[0x4000]
-        unsigned char ROM1[0x4000]
-        unsigned char VRAM[0x1000]
+    cdef:
+        unsigned char[0x100]  BOOT 
+        unsigned char[0x4000] ROM0 
+        unsigned char[0x4000] ROM1 
+        unsigned char[0x2000] VRAM 
+        unsigned char[0x2000] ERAM 
+        unsigned char[0x1000] WRAMlo 
+        unsigned char[0x1000] WRAMhi 
+
+        unsigned char[0xa0] OAM
+        unsigned char[0x7f] HRAM
+
+        MemoryEntry[0x10000] MMAP
 
     cdef inline unsigned char read8(MEM self, unsigned short address):
-        if address < 0x100:
-            return self.BOOT[address]
-        return 0
+        cdef MemoryEntry entry = self.MMAP[address]
+        if entry.read:
+            return entry.read_ptr.data[0]
+        elif entry.read_ptr.callback is not NULL:
+            return entry.read_ptr.callback(self)
+        return 0xff  # default for bad reads
 
     cdef inline unsigned short read16(MEM self, unsigned short address):
         cdef unsigned short value
-        if address < 0x100:
-            return read16(&self.BOOT[address])
-        return 0
+        value = self.read8(address)
+        value |= (<unsigned short>self.read8(address + 1)) << 8
+        return value
 
     cdef inline void write8(MEM self, unsigned short address, unsigned char value):
-        pass
+        cdef MemoryEntry entry = self.MMAP[address]
+        if entry.write:
+            entry.write_ptr.data[0] = value
+        elif entry.write_ptr.callback is not NULL:
+            entry.write_ptr.callback(self, value)
+
+    cdef inline void write16(MEM self, unsigned short address, unsigned short value):
+        self.write8(address, <unsigned char>value)
+        self.write8(address, <unsigned char>(value >> 8))
--- src/mem/mem.pyx
+++ src/mem/mem.pyx
@@ -1,2 +1,83 @@
+cdef inline MemoryEntry MakeRW(unsigned char* data):
+    cdef MemoryEntry entry 
+    entry.read_ptr.data = data
+    entry.read = True
+    entry.write_ptr.data = data
+    entry.write = True
+    return entry
+
+cdef inline MemoryEntry MakeROM(unsigned char* data):
+    cdef MemoryEntry entry 
+    entry.read_ptr.data = data
+    entry.read = True
+    entry.write_ptr.callback = NULL
+    entry.write = False
+    return entry
+
+cdef inline MemoryEntry MakeUnused():
+    cdef MemoryEntry entry 
+    entry.read_ptr.data = NULL
+    entry.read = False
+    entry.write_ptr.callback = NULL
+    entry.write = False
+    return entry
+
+cdef unsigned char read_unimplemented(MEM mem):
+    print("read from unimplemented address")
+    quit(-1)
+
+cdef void write_unimplemented(MEM mem, unsigned char value):
+    print("write to unimplemented address")
+    quit(-2)
+
+cdef inline MemoryEntry MakeUnimplemented():
+    cdef MemoryEntry entry 
+    entry.read_ptr.callback = read_unimplemented
+    entry.read = False
+    entry.write_ptr.callback = write_unimplemented
+    entry.write = False
+    return entry
+
 cdef class MEM:
-    pass
+    
+    def __cinit__(self):
+        cdef unsigned int i
+        for i in range(0x4000):
+            self.MMAP[i] = MakeROM(&self.ROM0[i])
+        
+        # if we want to use the boot rom
+        for i in range(0x100):
+            self.MMAP[i] = MakeROM(&self.BOOT[i])
+
+        for i in range(0x4000):
+            self.MMAP[0x4000 + i] = MakeROM(&self.ROM1[i])
+
+        for i in range(0x2000):
+            self.MMAP[0x8000 + i] = MakeRW(&self.VRAM[i])
+
+        for i in range(0x2000):
+            self.MMAP[0xa000 + i] = MakeRW(&self.ERAM[i])
+
+        for i in range(0x1000):
+            self.MMAP[0xc000 + i] = MakeRW(&self.WRAMlo[i])
+        for i in range(0x1000):
+            self.MMAP[0xd000 + i] = MakeRW(&self.WRAMhi[i])
+
+        for i in range(0x1000):  # ECHO RAM
+            self.MMAP[0xe000 + i] = MakeRW(&self.WRAMlo[i])
+        for i in range(0xe00):   # ECHO RAM
+            self.MMAP[0xf000 + i] = MakeRW(&self.WRAMhi[i])
+
+        for i in range(0xa0):
+            self.MMAP[0xfe00 + i] = MakeRW(&self.OAM[i])
+
+        for i in range(0x60):
+            self.MMAP[0xfea0 + i] = MakeUnused()
+
+        for i in range(0x80):   # IO
+            self.MMAP[0xff00 + i] = MakeUnimplemented()
+
+        for i in range(0x7f):
+            self.MMAP[0xff80 + i] = MakeRW(&self.HRAM[i])
+        self.MMAP[0xffff] = MakeUnimplemented()  # IE
+            
